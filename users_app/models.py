b66b93ef6520cba3bf10a121d06479dd45268f63
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-08 15:26:36 +0200

    passed Avatar model to separate app

---
+++ users_app/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class AvatarProfile(models.Model):
+    """To keep avatar of user"""
+    user = models.OneToOneField(User)
+
+    class Meta(object):
+        verbose_name = u"Avatar of User"
+
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name='AvataR')
+
+    def __unicode__(self):
+        return self.user.email
